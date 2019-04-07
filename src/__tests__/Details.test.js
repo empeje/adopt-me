b4e5ec1c73984e6814a785fe10d81baf3471e02b
mpj <11813607+empeje@users.noreply.github.com>
2019-04-07 13:05:52 +0800

    Add test for toggleModal

--- src/__tests__/Details.test.js
+++ src/__tests__/Details.test.js
@@ -2,7 +2,18 @@ import React from "react";
 import { create } from "react-test-renderer";
 import Details from "../Details";
 
-test("snpashot", () => {
-  const c = create(<Details />);
-  expect(c.toJSON()).toMatchSnapshot();
+describe("Details page", () => {
+  test("snpashot", () => {
+    const c = create(<Details />);
+    expect(c.toJSON()).toMatchSnapshot();
+  });
+
+  test("shows modal when toggleModal is called", () => {
+    const c = create(<Details search={() => {}} />);
+    const instance = c.getInstance();
+
+    expect(instance.state.showModal).toBe(false);
+    instance.toggleModal();
+    expect(instance.state.showModal).toBe(true);
+  });
 });
